fd817b44ea823e0334ad87ac711777ecb071d78d
EverPT47 <everfelipepinto@hotmail.com>
2024-10-22 10:35:16 -0500

    Initial commit of Laravel project

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () { 
+    return view('home');
+});
+
+Route::get('blog', function () { 
+    // consulta bd avanz
+    $posts=[
+        ['id'=>1,'title'=>'PHP',    'slug'=>'php'],
+        ['id'=>2,'title'=>'Laravel','slug'=>'laravel']
+
+    ];
+    return view('blog',['posts'=>$posts]);
+});
+
+Route::get('blog/{slug}', function ($slug) { 
+    $post = $slug;
+    
+    return view('post',['post'=>$post]);
+});
